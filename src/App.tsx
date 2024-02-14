e7f5d56fa9448d75192f8b03e75a9e65afe1a864
Anton Forikov <aforikov@platformanetworks.com>
2024-02-14 13:53:25 +0600

    Added request to database and element for perform it

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import AppBar from './components/AppBar/AppBar';
 import {Route, Routes} from 'react-router-dom';
 import Home from './containers/Home/Home';
-import Add from './containers/Add/Add';
-import About from './containers/About/About';
-import Contacts from './containers/Contacts/Contacts';
-import Post from './containers/Post/Post';
+import Page from './containers/Page/Page';
 
 function App() {
 
@@ -14,13 +11,14 @@ function App() {
       <div className="container-fluid">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<Home />} />
-          <Route path="/add" element={<Add edit={false} />} />
-          <Route path="/posts/:id" element={<Post/>} >
-            <Route path="edit" element={<Add edit={true} />}/>
-          </Route>
-          <Route path="/about" element={<About />} />
-          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/pages/:pageName" element={<Page />}/>
+          {/*<Route path="/posts" element={<Home />} />*/}
+          {/*<Route path="/add" element={<Add edit={false} />} />*/}
+          {/*<Route path="/posts/:id" element={<Post/>} >*/}
+          {/*  <Route path="edit" element={<Add edit={true} />}/>*/}
+          {/*</Route>*/}
+          {/*<Route path="/about" element={<About />} />*/}
+          {/*<Route path="/contacts" element={<Contacts />} />*/}
           <Route path="*" element={<h1>Not found</h1>} />
         </Routes>
       </div>
--- src/components/AppBar/AppBar.tsx
+++ src/components/AppBar/AppBar.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, {useCallback, useEffect, useState} from 'react';
 import {NavLink} from 'react-router-dom';
+import {PagesApi} from '../../types.d.';
+import axiosAPI from '../../axiosAPI';
+import Spinner from '../Spinner/Spinner';
 
 const AppBar: React.FC = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [pages, setPages] = useState<string[]>([]);
+
+  const getData = useCallback(async () => {
+    try {
+      const response = await axiosAPI.get<PagesApi | null>('/pages.json');
+      const pagesApi = response.data;
+
+      if (!pagesApi) {
+        alert("This endpoint is empty.");
+      }
+
+      if (pagesApi) {
+        const pageNamesArray = Object.keys(pagesApi);
+        setPages(pageNamesArray);
+      } else {
+        setPages([]);
+      }
+    } catch {
+      alert ('Please check requested URL.');
+    } finally {
+      setIsLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    void getData();
+  }, [getData]);
+
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary px-3">
       <div className="container-md">
-        <NavLink className="navbar-brand" to="/posts">My Blog</NavLink>
+        <NavLink className="navbar-brand" to="/pages">Pages</NavLink>
       </div>
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/">Home</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/add">Add</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/about">About</NavLink>
-        </li>
-        <li className="nav-item">
-          <NavLink className="nav-link" to="/contacts">Contacts</NavLink>
-        </li>
-      </ul>
+      {isLoading ? <Spinner/> :
+        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+          {pages.map(page => {
+            return (
+              <li className="nav-item" key={Math.random()}>
+                <NavLink className="nav-link" to={`/pages/${page}`}>{page[0].toLocaleUpperCase()+page.slice(1)}</NavLink>
+              </li>
+            );
+          })}
+        </ul>
+      }
+
     </nav>
   );
 };
